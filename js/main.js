6ce0e9f742a50e43024c252cde4729d77d093055
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2021-06-09 10:42:09 +0200

    carousel finished

--- js/main.js
+++ js/main.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
         $nav.toggleClass('collapse');
     })
 
+    //owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        dots:false,
+        nav: true,
+        nevText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+});
+
 });
